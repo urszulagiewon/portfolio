b97b2c084720dd09c4a4421e460b97e747457f8b
@urszula.giewon <urszula giewon@gmail.com>
2019-03-28 19:13:43 +0100

    Optymaliczacja kodu js

--- js/main.js
+++ js/main.js
@@ -19,80 +19,36 @@ $(function () {
         }, 500, 'linear');
     });
 
-    /* show numbers*/
-    function unhideElement(toHide, toShow) {
-        toHide.toggle();
-        toShow.show();
+    /* me in numbers*/
+    function meInNumbers() {
+        
+            $('.column img').click(function () {
+                $(this).hide();
+                $(this).next('p').toggle();
+            });
+        
+            $('.column p').click(function () {
+                $(this).hide();
+                $(this).prev('.column img').show();
+            })
+        
     }
 
-    $(".one img").on("click", function () {
-        unhideElement($(".one img"), $(".one p"));
-    });
-    $(".one p").on("click", function () {
-        unhideElement($(".one p"), $(".one img"));
-    });
-    $(".two img").on("click", function () {
-        unhideElement($(".two img"), $(".two p"));
-    });
-    $(".two p").on("click", function () {
-        unhideElement($(".two p"), $(".two img"));
-    });
-    $(".three img").on("click", function () {
-        unhideElement($(".three img"), $(".three p"));
-    });
-    $(".three p").on("click", function () {
-        unhideElement($(".three p"), $(".three img"));
-    });
-    $(".four img").on("click", function () {
-        unhideElement($(".four img"), $(".four p"));
-    });
-    $(".four p").on("click", function () {
-        unhideElement($(".four p"), $(".four img"));
-    });
+    meInNumbers();
 
     /* Display exprience */
-    function displayElement(toHide, showOne, showTwo) {
-        toHide.hide();
-        showOne.show(1000);
-        showTwo.show(1000);
+    function showExperience() {
+        $('.show-more').on('click', function () {
+            $(this).closest('.job').find('.more').show();
+            $(this).hide();
+        })
+
+        $('.hide').on('click', function () {
+            $(this).closest('.job').find('.show-more').show();
+            $(this).closest('.more').hide();
+        })
     }
-
-    function hideElement(toShow, hideOne, hideTwo) {
-        toShow.show();
-        hideOne.hide(1000);
-        hideTwo.hide(1000);
-    }
-    /*Show more*/
-    $(".comperia .show-more").on("click", function () {
-        displayElement($(".comperia .show-more"), $(".comperia .extra-text"), $(".comperia .hide"));
-    });
-
-    $(".comperia .hide").on("click", function () {
-        hideElement($(".comperia .show-more"), $(".comperia .extra-text"), $(".comperia .hide"));
-    });
-
-    $(".medical .show-more").on("click", function () {
-        displayElement($(".medical .show-more"), $(".medical .extra-text"), $(".medical .hide"));
-    });
-
-    $(".medical .hide").on("click", function () {
-        hideElement($(".medical .show-more"), $(".medical .extra-text"), $(".medical .hide"));
-    });
-    $(".jula .show-more").on("click", function () {
-        displayElement($(".jula .show-more"), $(".jula .extra-text"), $(".jula .hide"));
-    });
-
-    $(".jula .hide").on("click", function () {
-        hideElement($(".jula .show-more"), $(".jula .extra-text"), $(".jula .hide"));
-    });
-
-    $(".verte .show-more").on("click", function () {
-        displayElement($(".verte .show-more"), $(".verte .extra-text"), $(".verte .hide"));
-    });
-
-    $(".verte .hide").on("click", function () {
-        hideElement($(".verte .show-more"), $(".verte .extra-text"), $(".verte .hide"));
-    });
+    showExperience();
 
     /*Grafik - front end developer*/
     function switchJob() {
